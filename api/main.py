7d03e250dbd6dfaac62e0cca2afad0e84a308c36
Lyreplus <lyreplus@gmail.com>
2024-11-08 22:50:22 +0100

    defined vehicle and request classes

--- api/main.py
+++ api/main.py
@@ -23,3 +23,23 @@ def calc_route():
         return jsonify({"message": "Received and saved data!"}), 200
     else:
         return jsonify({"error": "Error: the request doesn't contain json"}), 404
+
+class Vehicle:
+    def __init__(self, id, start_location, capacity_walking, capacity_wheelchair, capacity_stretcher, capacity_white_cross, max_capacity):
+        self.id = id
+        self.start_location = start_location
+        self.capacity_walking = capacity_walking
+        self.capacity_wheelchair = capacity_wheelchair
+        self.capacity_stretcher = capacity_stretcher
+        self.capacity_white_cross = capacity_white_cross
+        self.max_capacity = max_capacity
+
+    
+    
+class Request:
+    def __init__(self, id, accompanied, pickup_location, destionation, appointment_time):
+        self.id = id
+        self.accompanied = accompanied
+        self.pickup_location = pickup_location
+        self.destionation = destionation
+        self.appointment_time = appointment_time
