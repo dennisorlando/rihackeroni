9d3c34036679a24cf7181dd968f0cd485a28e751
Lyreplus <lyreplus@gmail.com>
2024-11-08 22:57:15 +0100

    add deserializer

--- api/main.py
+++ api/main.py
@@ -2,8 +2,8 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-vehicle_list = {}
-patient_list = {}
+vehicle_list = []
+request_list = []
 
 
 @app.route('/')
@@ -14,15 +14,17 @@ def index():
 @app.get('/routes')
 def calc_route():
     global vehicle_list
+    global request_list
     if request.is_json:
         data = request.get_json()
         if "vehicle_list" in data:
-            vehicle_list.update(data["vehicle_list"])
+            vehicle_list = [Vehicle.from_dict(v) for v in data["vehicle_list"]]
         if "patient_list" in data:
-            patient_list.update(data["patient_list"])
+            request_list = [Request.from_dict(p) for p in data["patient_list"]]
         return jsonify({"message": "Received and saved data!"}), 200
     else:
         return jsonify({"error": "Error: the request doesn't contain json"}), 404
+
 
 class Vehicle:
     def __init__(self, id, start_location, capacity_walking, capacity_wheelchair, capacity_stretcher, capacity_white_cross, max_capacity):
@@ -34,8 +36,20 @@ class Vehicle:
         self.capacity_white_cross = capacity_white_cross
         self.max_capacity = max_capacity
 
-    
-    
+    @classmethod
+    def from_dict(cls, data: dict):
+        return cls(
+            id=data["id"],
+            start_location=data["start_location"],
+            capacity_walking=data["capacity_walking"],
+            capacity_wheelchair=data["capacity_wheelchair"],
+            capacity_stretcher=data["capacity_stretcher"],
+            capacity_white_cross=data["capacity_white_cross"],
+            max_capacity=data["max_capacity"]
+        )
+
+
+
 class Request:
     def __init__(self, id, accompanied, pickup_location, destionation, appointment_time):
         self.id = id
@@ -43,3 +57,14 @@ class Request:
         self.pickup_location = pickup_location
         self.destionation = destionation
         self.appointment_time = appointment_time
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        return cls(
+            id=data["id"],
+            accompanied=data["accompanied"],
+            pickup_location=data["pickup_location"],
+            destionation=data["destionation"],
+            appointment_time=data["appointment_time"]
+        )
+
